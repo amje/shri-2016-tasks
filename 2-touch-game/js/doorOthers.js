8d88d4e5bc7fef37202af04bc0314cbd983e6cb4
Andrey Morozov <luckytrue@yandex.ru>
2016-07-10 19:52:36 +0300

    implement second door

--- 2-touch-game/js/doorOthers.js
+++ 2-touch-game/js/doorOthers.js
@@ -63,10 +63,98 @@ function Door1(number, onUnlock) {
     DoorBase.apply(this, arguments);
 
     // ==== Напишите свой код для открытия второй двери здесь ====
-    // Для примера дверь откроется просто по клику на неё
-    this.popup.addEventListener('click', function() {
-        this.unlock();
-    }.bind(this));
+    var container = this.popup.querySelector('.locks-riddle');
+    var locks = [].slice.call(this.popup.querySelectorAll('.locks-riddle__lock'));
+    var keys = [].slice.call(this.popup.querySelectorAll('.locks-riddle__key'));
+    var dropAreas = [];
+    var touchData = {};
+    var DROPPED_TIMEOUT = 200;
+
+    keys.forEach(function(key) {
+        key.addEventListener('pointerdown', _onKeyPointerDown.bind(this));
+    }, this);
+
+    locks.forEach(function(lock) {
+        var rect = lock.getBoundingClientRect();
+        dropAreas.push({
+            fromX: rect.left,
+            toX: rect.right,
+            fromY: rect.top,
+            toY: rect.bottom,
+            color: lock.classList.contains('locks-riddle__lock_color_yellow') ? 'yellow' : 'blue',
+            dropped: false
+        });
+    });
+
+    container.addEventListener('pointermove', _.throttle(_onContainerPointerMove.bind(this), 50));
+    container.addEventListener('pointerup', _onContainerPointerUp.bind(this));
+    container.addEventListener('pointercancel', _onContainerPointerUp.bind(this));
+
+    function _onKeyPointerDown(e) {
+        // Запоминаем данные о нажатии на ключ
+        touchData[e.pointerId] = {
+            x: e.clientX,
+            y: e.clientY,
+            target: e.target,
+            color: e.target.classList.contains('locks-riddle__key_color_yellow') ? 'yellow' : 'blue'
+        };
+        e.target.classList.add('locks-riddle__key_dragging');
+    }
+
+    function _onContainerPointerMove(e) {
+        var touch = touchData[e.pointerId];
+
+        if (!touch) {
+            return;
+        }
+        touch.target.style.transform = 'translate(' + [e.clientX - touch.x, e.clientY - touch.y].join('px, ') + 'px)';
+    }
+
+    function _onContainerPointerUp(e) {
+        var data = touchData[e.pointerId];
+
+        if (!data) {
+            return;
+        }
+
+        // Проверяем правильно ли отпущен ключ
+        var droppedArea = dropAreas.filter(function(dropArea) {
+            return !dropArea.dropped &&
+                dropArea.color === data.color &&
+                e.clientX >= dropArea.fromX && e.clientX <= dropArea.toX &&
+                e.clientY >= dropArea.fromY && e.clientY <= dropArea.toY;
+        })[0];
+
+        if (droppedArea) {
+            droppedArea.dropped = true;
+            setTimeout(function() {
+                droppedArea.dropped = false;
+                restoreTouchData(e.pointerId);
+            }, DROPPED_TIMEOUT);
+        } else {
+            restoreTouchData(e.pointerId);
+        }
+
+        checkCondition.apply(this);
+    }
+
+    /**
+     * Восстановить началльное положение ключа по id нажатия
+     * @param pointerId - id нажатия
+     */
+    function restoreTouchData(pointerId) {
+        var data = touchData[pointerId];
+
+        data.target.style.transform = '';
+        data.target.classList.remove('locks-riddle__key_dragging');
+        delete touchData[pointerId];
+    }
+
+    function checkCondition() {
+        if (dropAreas.every(function(dropArea) { return dropArea.dropped; })) {
+            this.unlock();
+        }
+    }
     // ==== END Напишите свой код для открытия второй двери здесь ====
 }
 Door1.prototype = Object.create(DoorBase.prototype);
