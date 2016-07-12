43fd472e42874faffb97a2b784ec4840babecf30
Andrey Morozov <luckytrue@yandex.ru>
2016-07-12 22:45:07 +0300

    update door 1

--- 2-touch-game/js/doorOthers.js
+++ 2-touch-game/js/doorOthers.js
@@ -63,15 +63,17 @@ function Door1(number, onUnlock) {
     DoorBase.apply(this, arguments);
 
     // ==== Напишите свой код для открытия второй двери здесь ====
-    var container = this.popup.querySelector('.locks-riddle');
     var locks = [].slice.call(this.popup.querySelectorAll('.locks-riddle__lock'));
     var keys = [].slice.call(this.popup.querySelectorAll('.locks-riddle__key'));
     var dropAreas = [];
-    var touchData = {};
-    var DROPPED_TIMEOUT = 200;
+    var pointerData = {};
+    var DROP_TIMEOUT = 50;
 
     keys.forEach(function(key) {
         key.addEventListener('pointerdown', _onKeyPointerDown.bind(this));
+        key.addEventListener('pointermove', _onContainerPointerMove.bind(this));
+        key.addEventListener('pointerup', _onContainerPointerUp.bind(this));
+        key.addEventListener('pointercancel', _onContainerPointerUp.bind(this));
     }, this);
 
     locks.forEach(function(lock) {
@@ -86,32 +88,34 @@ function Door1(number, onUnlock) {
         });
     });
 
-    container.addEventListener('pointermove', _onContainerPointerMove.bind(this));
-    container.addEventListener('pointerup', _onContainerPointerUp.bind(this));
-    container.addEventListener('pointercancel', _onContainerPointerUp.bind(this));
-
     function _onKeyPointerDown(e) {
-        // Запоминаем данные о нажатии на ключ
-        touchData[e.pointerId] = {
-            x: e.clientX,
-            y: e.clientY,
+        pointerData[e.pointerId] = {
+            startX: e.clientX,
+            startY: e.clientY,
             target: e.target,
             color: e.target.classList.contains('locks-riddle__key_color_yellow') ? 'yellow' : 'blue'
         };
         e.target.classList.add('locks-riddle__key_dragging');
+        e.target.setPointerCapture(e.pointerId);
     }
 
     function _onContainerPointerMove(e) {
-        var touch = touchData[e.pointerId];
+        var pointer = pointerData[e.pointerId];
 
-        if (!touch) {
+        if (!pointer) {
             return;
         }
-        touch.target.style.transform = 'translate(' + [e.clientX - touch.x, e.clientY - touch.y].join('px, ') + 'px)';
+
+        pointer.currentX = e.clientX;
+        pointer.currentY = e.clientY;
+        updatePosition(pointer);
     }
 
     function _onContainerPointerUp(e) {
-        var data = touchData[e.pointerId];
+        var pointerId = e.pointerId;
+        var data = pointerData[pointerId];
+
+        e.target.releasePointerCapture(e.pointerId);
 
         if (!data) {
             return;
@@ -121,33 +125,34 @@ function Door1(number, onUnlock) {
         var droppedArea = dropAreas.filter(function(dropArea) {
             return !dropArea.dropped &&
                 dropArea.color === data.color &&
-                e.clientX >= dropArea.fromX && e.clientX <= dropArea.toX &&
-                e.clientY >= dropArea.fromY && e.clientY <= dropArea.toY;
+                data.currentX >= dropArea.fromX && data.currentX <= dropArea.toX &&
+                data.currentY >= dropArea.fromY && data.currentY <= dropArea.toY;
         })[0];
 
         if (droppedArea) {
             droppedArea.dropped = true;
+            checkCondition.apply(this);
             setTimeout(function() {
                 droppedArea.dropped = false;
-                restoreTouchData(e.pointerId);
-            }, DROPPED_TIMEOUT);
+                restorePosition(pointerId);
+            }, DROP_TIMEOUT);
         } else {
-            restoreTouchData(e.pointerId);
+            restorePosition(pointerId);
         }
-
-        checkCondition.apply(this);
     }
 
-    /**
-     * Восстановить началльное положение ключа по id нажатия
-     * @param pointerId - id нажатия
-     */
-    function restoreTouchData(pointerId) {
-        var data = touchData[pointerId];
+    function updatePosition(pointer) {
+        requestAnimationFrame(function() {
+            pointer.target.style.transform = 'translate(' + [pointer.currentX - pointer.startX, pointer.currentY - pointer.startY].join('px, ') + 'px)';
+        });
+    }
 
-        data.target.style.transform = '';
-        data.target.classList.remove('locks-riddle__key_dragging');
-        delete touchData[pointerId];
+    function restorePosition(pointerId) {
+        requestAnimationFrame(function() {
+            var data = pointerData[pointerId];
+            data.target.style.transform = '';
+            data.target.classList.remove('locks-riddle__key_dragging');
+        });
     }
 
     function checkCondition() {
@@ -155,7 +160,6 @@ function Door1(number, onUnlock) {
             this.unlock();
         }
     }
-
     // ==== END Напишите свой код для открытия второй двери здесь ====
 }
 Door1.prototype = Object.create(DoorBase.prototype);
