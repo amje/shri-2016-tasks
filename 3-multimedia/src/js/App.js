43fd34e7ae1c8176e33bec78fefe5df5ead3d821
Andrey Morozov <luckytrue@yandex.ru>
2016-07-18 00:45:53 +0300

    try to add subs

--- 3-multimedia/src/js/App.js
+++ 3-multimedia/src/js/App.js
@@ -8,15 +8,20 @@ class App {
 
         // For testing
         this.handleFormSubmit({
-            video: 'http://localhost:8080/kinopoisk.ru-Sherlock-284167.mp4',
+            video: 'kinopoisk.ru-Sherlock-284167.mp4',
             subs: 'https://raw.githubusercontent.com/shri-msk-2016/dz-multimedia/master/subs.srt',
             audio: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Maple_Leaf_RagQ.ogg'
         });
     }
 
-    handleFormSubmit(data) {
+    handleFormSubmit(urls) {
         this.showPlayerPage();
-        this.player.init(data);
+        this.getSubs(urls.subs)
+            .then(parseSubs)
+            .then((parsedSubs) => {
+                this.player.init(urls.video, urls.audio, parsedSubs);
+            })
+            .catch((e) => console.log(e));
     }
 
     showPlayerPage() {
@@ -27,6 +32,44 @@ class App {
         pagesArray.forEach((page) => page.classList.add('page_hidden'));
         playerPage.classList.remove('page_hidden');
     }
+
+    getSubs(url) {
+        return new Promise((resolve) => {
+            const xhr = new XMLHttpRequest();
+            xhr.addEventListener('readystatechange', () => {
+                if (xhr.readyState !== 4) {
+                    return;
+                }
+
+                resolve(xhr.responseText);
+            });
+            xhr.open('GET', url);
+            xhr.send(null);
+        });
+    }
+}
+
+function parseSubs(data) {
+    return data.split('\n\n').map((subsChunk) => {
+        let [, time, ...text] = subsChunk.split('\n');
+        text = text.join('\n');
+        const [from, to] = time.split(' --> ');
+        const fromSeconds = subTimeStringToSeconds(from);
+        const toSeconds = subTimeStringToSeconds(to);
+        const duration = Math.round((toSeconds - fromSeconds) * 1e3) / 1e3;
+
+        return {
+            start: Math.round(toSeconds * 1e3) / 1e3,
+            duration,
+            text
+        };
+    });
+}
+
+function subTimeStringToSeconds(timestring) {
+    const [hh, mm, ss, ms] = timestring.split(/[:,]/);
+
+    return hh * 3600 + mm * 60 + ss * 1 + ms / 1000;
 }
 
 const app = new App();
--- 3-multimedia/src/js/Player.js
+++ 3-multimedia/src/js/Player.js
@@ -1,3 +1,5 @@
+const CANVAS_WIDTH = 600;
+
 export default class Player {
     constructor() {
         this.element = document.querySelector('.player');
@@ -10,6 +12,7 @@ export default class Player {
         this.ctx = this.canvas.getContext('2d');
 
         this.playButton.addEventListener('click', this.handlePlayButtonClick.bind(this));
+        this.video.addEventListener('loadedmetadata', this.handleVideoMetaLoaded.bind(this));
         this.video.addEventListener('timeupdate', this.handleVideoTimeUpdate.bind(this));
         this.video.addEventListener('ended', this.handleVideoEnded.bind(this));
         this.progressBar.addEventListener('click', this.handleProgressBarClick.bind(this));
@@ -29,46 +32,78 @@ export default class Player {
         }
     }
 
+    handleVideoMetaLoaded() {
+        this.setDimensions();
+    }
+
     handleVideoTimeUpdate() {
         requestAnimationFrame(() => {
             this.progressPlayed.style.transform = `scaleX(${this.video.currentTime / this.video.duration})`;
+
+            const nextSub = this.subs[this.subIndex];
+            if (nextSub && this.video.currentTime >= nextSub.start) {
+                this.showSub(nextSub);
+            }
         });
     }
 
     handleVideoEnded() {
+        this.audio.pause();
+        this.audio.currentTime = 0;
         this.playing = false;
+        this.subIndex = 0;
         this.playButton.classList.remove('play-button_pause');
     }
 
     handleProgressBarClick(e) {
         const rect = e.currentTarget.getBoundingClientRect();
+        const time = (e.clientX - rect.left) / rect.width * this.video.duration;
 
-        this.video.currentTime = (e.clientX - rect.left) / rect.width * this.video.duration;
+        this.seekTo(time);
     }
 
     play() {
+        this.audio.play();
         this.video.play();
         this.playing = true;
         requestAnimationFrame(this.render);
     }
 
     pause() {
+        this.audio.pause();
         this.video.pause();
         this.playing = false;
     }
 
-    init(urls) {
-        this.video.src = urls.video;
+    seekTo(time) {
+        this.audio.currentTime = time;
+        this.video.currentTime = time;
+    }
+
+    showSub(sub) {
+        this.sub = sub;
+        this.subIndex++;
+        this.video.pause();
+        setTimeout(() => {
+            this.video.play();
+            this.sub = null;
+        }, sub.duration * 1000);
+    }
+
+    init(videoSrc, audioSrc, subs) {
+        console.log(subs);
+        this.video.src = videoSrc;
+        this.audio.src = audioSrc;
         this.video.volume = 0;
-        this.video.addEventListener('loadedmetadata', () => {
-            this.setDimensions();
-        });
+        this.audio.volume = 0.2;
+        this.subs = subs;
+        this.subIndex = 0;
     }
 
     setDimensions() {
         const ratio = this.canvas.clientWidth / this.canvas.clientHeight;
 
-        this.width = 300;
+        this.width = CANVAS_WIDTH;
         this.height = this.width / ratio;
 
         this.canvas.width = this.width;
@@ -76,16 +111,33 @@ export default class Player {
     }
 
     render() {
-        if (!this.playing) {
+        if (!this.playing && !this.sub) {
             return;
         }
 
-        let frame = this.getFrame();
+        if (this.sub) {
+            this.ctx.fillStyle = '#111';
+            this.ctx.fillRect(0, 0, this.width, this.height);
+            // TODO: Вынести в функцию
+            let fontSize = this.height / 14;
+            let lineHeight = fontSize * 1.2;
+            this.ctx.font = `${fontSize}px Oranienbaum`;
+            this.ctx.textBaseline = 'middle';
+            this.ctx.fillStyle = '#fff';
+            this.sub.text.split('\n').forEach((line, index, text) => {
+                let length = text.length;
+                let offset = (index - length / 2 + 0.5) * lineHeight;
 
-        frame = this.desaturateFrame(frame);
+                this.ctx.fillText(line, 20, this.height / 2 + offset);
+            });
+        } else {
+            let frame = this.getFrame();
+
+            frame = this.desaturateFrame(frame);
+            this.ctx.putImageData(frame, 0, 0);
+        }
         // TODO: Эффект царапин, зернистость пленки
 
-        this.ctx.putImageData(frame, 0, 0);
         requestAnimationFrame(this.render);
     }
 
